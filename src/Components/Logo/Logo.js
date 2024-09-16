dbed5ace447647e1a80ed2cb84022322c7e66b1f
Joy Leila Kendi <muriungikendileila@gmail.com>
2024-09-16 09:52:43 +0300

    Resolve issue where home page displayed default name instead of user's name

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Tilt } from 'react-tilt';
+//import { Tilt } from 'react-tilt';
+import Tilt from 'react-parallax-tilt';
 import './Logo.css';
 import brain from './brain.png';
 
@@ -7,10 +8,12 @@ import brain from './brain.png';
 const Logo= () => {
     return(
         <div className='ma4 mt0'>
-            <Tilt className='Tilt br3 shadow-2' options={{max: 55}} style={{ height: 150, width: 150 }}>
-                <div className='Tilt-inner pa3'><img style={{paddingTop:'5px', height: '100px'}} alt='brain' src={brain} /></div>
-            </Tilt>
-        </div>
+        <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+          <div className="Tilt-inner pa3">
+            <img style={{paddingTop: '5px', height:'100px' }} alt='logo' src={brain}/>
+          </div>
+        </Tilt>
+      </div>
     );
 }
 
