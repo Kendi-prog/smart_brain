2b41c4db69ba1ae3c0899b19adaeda8195e74000
Joy Leila Kendi <muriungikendileila@gmail.com>
2024-09-16 12:51:02 +0300

    update on the onSubmit Button and the particles

--- src/App.js
+++ src/App.js
@@ -10,23 +10,27 @@ import Register from './Components/Register/Register';
 import ParticlesBg from 'particles-bg';
 
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
+
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl:'',
-      box:{}, 
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -66,6 +70,38 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+      fetch('http://localhost:3000/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })
+      })
+      .then(response => response.json())
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+            .catch(console.log)
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+  }
+
+
+  /*onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
     
   ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -147,7 +183,7 @@ class App extends Component {
       })
       .catch(error => console.log('error', error));
 
-  }
+  }*/
 
   onRouteChange = (route) => {
     if(route === 'signout'){
