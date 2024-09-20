7e0ce4a554d5be8a828f75ff5ce8200bd89f10ed
Joy Leila Kendi <muriungikendileila@gmail.com>
2024-09-20 14:55:46 +0300

    code review

--- src/App.js
+++ src/App.js
@@ -48,17 +48,16 @@ class App extends Component {
   
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions.bounding_box;
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
-    const height = Number(image.heght);
+    const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
       rightCol: width - (clarifaiFace.right_col * width),
       bottomRow: height - (clarifaiFace.bottom_row * height)
     }
-    
   }
 
   displayFaceBox = (box) => {
